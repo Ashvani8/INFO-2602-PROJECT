c6a513553ba666864471bf3aac9292e3cf9d62fc
Ashvani8 <124642272+Ashvani8@users.noreply.github.com>
2024-04-21 22:50:34 -0400

    Update test_app.py

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -1,9 +1,9 @@
 import os, tempfile, pytest, logging, unittest
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from main import create_app
-
 from App.main import create_app
+
+
 from App.database import db, create_db
 from App.models import User
 from App.controllers import (
