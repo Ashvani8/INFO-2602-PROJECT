9d1aaa93002d3f1afe22298b02b0df2e2de74758
Ashvani8 <124642272+Ashvani8@users.noreply.github.com>
2024-04-21 22:56:08 -0400

    Update test_app.py

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -1,12 +1,11 @@
-import os, tempfile, pytest, logging, unittest
+import logging
+import unittest
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from App.main import create_app
-
-
-from App.database import db, create_db
-from App.models import User
-from App.controllers import (
+from ..App.main import create_app
+from ..App.database import db, create_db
+from ..App.models import User
+from ..App.controllers import (
     create_user,
     get_all_users_json,
     login,
@@ -14,7 +13,6 @@ from App.controllers import (
     get_user_by_username,
     update_user
 )
-
 
 LOGGER = logging.getLogger(__name__)
 
